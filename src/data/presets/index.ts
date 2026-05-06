656484c31236eaa09f4f841966b50ec07cebf151
wsw12321 <2183220676@qq.com>
2026-05-06 23:30:03 +0800

    feat(data): 完成了剩下的预设 Prompt 的编写

--- src/data/presets/index.ts
+++ src/data/presets/index.ts
@@ -1,13 +1,20 @@
-const p1 = require('./p1.json');
-const p2 = require('./p2.json');
-const p3 = require('./p3.json');
-const p4 = require('./p4.json');
-const p5 = require('./p5.json');
-const p6 = require('./p6.json');
-const p7 = require('./p7.json');
-const p8 = require('./p8.json');
+import p1 from './p1.json';
+import p2 from './p2.json';
+import p3 from './p3.json';
+import p4 from './p4.json';
+import p5 from './p5.json';
+import p6 from './p6.json';
+import p7 from './p7.json';
+import p8 from './p8.json';
+import p9 from './p9.json';
+import p10 from './p10.json';
+import p11 from './p11.json';
+import p12 from './p12.json';
+import p13 from './p13.json';
+import p14 from './p14.json';
+import p15 from './p15.json';
 
-export const presetFiles: Record<string, any> = {
+export const presetFiles: Record<string, unknown> = {
   p1,
   p2,
   p3,
@@ -16,4 +23,11 @@ export const presetFiles: Record<string, any> = {
   p6,
   p7,
   p8,
+  p9,
+  p10,
+  p11,
+  p12,
+  p13,
+  p14,
+  p15,
 };
